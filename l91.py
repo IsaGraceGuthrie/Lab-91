98c748a60ac0b3cec1970e297aaa599df767f73c
IsaGraceGuthrie <116840075+IsaGraceGuthrie@users.noreply.github.com>
2022-10-27 15:55:04 -0400

    Add files via upload

---
+++ l91.py
@@ -0,0 +1,9 @@
+lines=["Haiku frogs in snow",
+       "A limerick came from Natucket",
+       "Tetrametic drum-beats thrumming, Hiawathianic rhythm."]
+
+def breakify(x):
+    y= "<br>".join(x)
+    print(y)
+
+breakify(lines)
